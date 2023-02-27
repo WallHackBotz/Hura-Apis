2ca563262a387b25f3f411bbd1c7605130acc8a7
Rrsszxx <96643671+WallHackBotz@users.noreply.github.com>
2023-02-27 13:47:45 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Machiato'// yourname
+global.MONGO_DB_URI = "mongodb+srv://gendon-miksu:gendonmiksujaya32@cluster0.vaetpuu.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "zuxxyy28@gmail.com" //email
+global.email_password = "tiggetgndoxzuiec" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
